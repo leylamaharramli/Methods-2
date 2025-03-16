db75c0a6e18af5f577d04c620047c2289cd88732
leylamaharramli <leyla@example.com>
2025-03-16 15:40:05 +0400

    finding min num from the given arr

---
+++ task-4/Program.cs
@@ -0,0 +1,28 @@
+﻿
+#region
+//namespace Methods
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            Min([6, 12, 4, 10, 2, 8]);
+//        }
+
+//        static void Min(int[] array)
+//        {
+//            int[] arr = array;
+//            int min_num = arr[0];
+//            for (int i=1; i<arr.Length; i++)
+//            {
+//                if (min_num > arr[i])
+//                {
+//                    min_num = arr[i];
+//                }      
+//            }
+//            Console.WriteLine(min_num);
+//        }
+//    }
+//}
+#endregion
+
