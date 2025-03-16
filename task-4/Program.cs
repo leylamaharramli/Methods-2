ae807f42b664da5716eae37f5d9b8809a1c6e978
leylamaharramli <leyla@example.com>
2025-03-16 15:42:11 +0400

    finding areas of figures

--- task-4/Program.cs
+++ task-4/Program.cs
@@ -26,3 +26,58 @@
 //}
 #endregion
 
+#region
+//namespace Methods
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            Area(7);
+//            Area(4, 8);
+//            Area(3, 5, 9);
+//            Area(2, 4, 7, 3);
+//        }
+//        static void Area(double num)
+//        {
+//            double r = num;
+//            int pi = 3;
+//            double cevrenin_sahesi = pi * r * r;
+
+//            Console.WriteLine(cevrenin_sahesi);
+//        }
+
+//        static void Area(double num1, double num2)
+//        {
+//            double a = num1;
+//            double b = num2;
+//            double duzbucaqlinin_sahesi = a * b;
+
+//            Console.WriteLine(duzbucaqlinin_sahesi);
+//        }
+
+//        static void Area(double num1, double num2, double num3)
+//        {
+//            double a = num1;
+//            double b = num2;
+//            double c = num3;
+//            double duzbparalelpiped_tam_sethinin_sahesi = 2 * (a * c + b * c + a * c);
+
+//            Console.WriteLine(duzbparalelpiped_tam_sethinin_sahesi);
+//        }
+
+//        static void Area(double num1, double num2, double num3, double num4)
+//        {
+//            double a = num1;
+//            double b = num2;
+//            double c = num3;
+//            double r = num4;
+
+//            double p = (a + b + c) / 2;
+//            double ucbucaq_daxilinde_cevrenin_sahesi = p * r;
+
+//            Console.WriteLine(ucbucaq_daxilinde_cevrenin_sahesi);
+//        }
+//    }
+//}
+#endregion
